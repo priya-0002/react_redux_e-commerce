e82bdf5d01d26a7916fc9c59c3c04fa390dd4201
priya-002 <priya.md0002@gmail.com>
2022-01-08 23:52:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import './User';
 import User from './User';
 import Customer from './Customer'
-
+import HomeContainer from './containers/HomeContainer'
+import HeaderContainer from './containers/HeaderContainer'
 function App() {
   return (
     <div className="App">
-      <h1>App Component</h1> 
-      <User data={{name:'priya', age:21}} />
+<h1>App Component</h1> 
+     <User data={{name:'priya', age:21}} />
     <Customer data1={{name:'ajay', age:40}}/>
+   <HeaderContainer/>
+    <HomeContainer/>
     </div>
     
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,24 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Home </h1>
+           
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-11-2.jpg" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
         </div>
     )
 }
