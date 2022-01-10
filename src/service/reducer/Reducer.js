12761c403200d93ade7beecc6a22f31c37a04bc3
priya-002 <priya.md0002@gmail.com>
2022-01-10 23:04:06 +0530

    first commit

--- src/service/reducer/Reducer.js
+++ src/service/reducer/Reducer.js
@@ -1,13 +1,1 @@
-const initialState={
-    cardData:[]
-}
-export default function cardItems(initalState, action) {
-    switch(action.type){
-        case ADD_TO_CART:
-            return {
-                ...initialState,
-                cardData:action.data
-            }
-    }
-}
-    
+
